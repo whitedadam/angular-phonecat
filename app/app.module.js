32b79c63e5ad524c2d715a94de962250c1898714
Adam Whited <adam.whited@availity.com>
2022-05-16 14:08:08 -0400

    Step 11

--- app/app.module.js
+++ app/app.module.js
@@ -1,2 +1,2 @@
 // Define the `phonecatApp` module
-angular.module("phonecatApp", ["ngRoute", "phoneList", "phoneDetail"]);
+angular.module("phonecatApp", ["ngRoute", "core", "phoneList", "phoneDetail"]);
